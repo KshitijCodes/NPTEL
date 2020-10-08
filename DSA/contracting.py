e2e848928cb6dc52b8976db6d52b682731ad32bf
Kshitij Srivastava <46964552+KshitijCodes@users.noreply.github.com>
2020-10-08 14:57:46 +0530

    Update contracting.py

--- DSA/contracting.py
+++ DSA/contracting.py
@@ -1,7 +1,5 @@
 def contracting(l):
-  for i in range(1, len(l) - 1):
-    if abs(l[i] - l[i-1]) > abs(l[i + 1] - l[i]):
-      continue
-    else:
+  for i in range(1,len(l)-1):
+    if abs(l[i]-l[i-1])<=abs(l[i+1]-l[i]):
       return False
   return True
